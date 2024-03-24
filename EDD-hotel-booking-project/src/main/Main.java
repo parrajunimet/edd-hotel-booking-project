7cbda257c86baa03c050f4b0381064636caee367
lpp0810 <perez.luis@correo.unimet.edu.ve>
2024-03-24 00:15:51 -0400

    creado metodo en basic para leer el archivo reservaciones
    y armar el arbol BB

--- EDD-hotel-booking-project/src/main/Main.java
+++ EDD-hotel-booking-project/src/main/Main.java
@@ -10,13 +10,20 @@ import edd.*;
  */
 public class Main {
     
+    //Se hacen static para su accesado desde las interfaces a medida que se editan sus datos.
     public static Hashtable hash;
+    public static ABBReservaciones reservas;
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //El basic functions es la columna vertebral de esto.
         BasicFunctions func = new BasicFunctions();
+        
+        // Reservaciones
+        reservas = func.Reservas();
         
         // Estado Actual
         Lista<Client> guests = func.Estado();
